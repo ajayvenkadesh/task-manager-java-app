4bc72f5bbe0e6d9372459781497a90ff6e058a42
Ajayvenkadesh KS <ajaynithin0404@gmail.com>
2024-11-02 22:06:36 +0530

    add

---
+++ src/main/java/com/jap/task/User.java
@@ -0,0 +1,40 @@
+package com.jap.task;
+
+public class User {
+
+    String userName,password;
+
+    //declare the attribute userName,password
+    public User(String userName, String password) {
+
+        this.userName=userName;
+        this.password=password;
+
+    }
+  //declare the getter setter and toString methods
+
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "userName='" + userName + '\'' +
+                ", password='" + password + '\'' +
+                '}';
+    }
+}
